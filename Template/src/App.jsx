1700c17d1679db06a2e2445d4542f7b9b2221df4
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2024-01-15 10:10:12 +0100

    Summary

--- Template/src/App.jsx
+++ Template/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Footer } from './components/footer/Footer.jsx';
 import { Home, Advokaterne, Kontakt, Om } from './pages'
 import { Nav } from './components'
-import { Header } from './components/header/header.jsx'
+import { PageError } from './components/PageError/PageError.jsx';
 import './App.scss'
 
 
@@ -16,14 +16,14 @@ function App() {
       
      <Router>
       <Nav />
-      <Header></Header>
       <Routes>
+        <Route path='*' Component={PageError} />
         <Route path="/" Component={Home} />
         <Route path="/advokaterne" Component={Advokaterne} />
         <Route path="/om" Component={Om} />
         <Route path="/kontakt" Component={Kontakt} />
       </Routes>
-        <Footer></Footer>
+        <Footer />
      </Router>
     </>
   )
--- Template/src/pages/home/home.jsx
+++ Template/src/pages/home/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Section_cards } from '../../components'
+import { Header } from '../../components/header/header'
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <Section_cards />
     </div>
   )
